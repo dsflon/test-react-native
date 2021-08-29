15cc6d116ecbedd2c1fc08677bfbe8d3b72184ed
Daiki Saito <dsflon@gmail.com>
2021-08-29 22:06:36 +0900

    styling

--- App.tsx
+++ App.tsx
@@ -1,32 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
+// import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
-import Hello from './src/components/Hello';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Hello
-        test={12}
-        bool
-      >
-        World
-      </Hello>
+      <View style={styles.appBar}>
+        <Text style={styles.appBarTitle}>Memo App</Text>
+        <Text style={styles.appBarLogout}>Logout</Text>
+      </View>
+      {/* header */}
 
-      <Hello
-        additinalStyle={{
-          fontSize: 20,
-          color: '#fff',
-          backgroundColor: 'blue',
-        }}
-      >
-        World
-      </Hello>
+      <View>
+        <View style={styles.list}>
+          <View>
+            <Text style={styles.listTitle}>買い物リスト</Text>
+            <Text style={styles.listDate}>2021年8月29日 19:00</Text>
+          </View>
+          <View>
+            <Text style={styles.listDelete}>✕</Text>
+          </View>
+        </View>
+        {/* item end */}
 
-      <Text>Open up App.tsx to start working on your app!</Text>
+        <View style={styles.list}>
+          <View>
+            <Text style={styles.listTitle}>買い物リスト</Text>
+            <Text style={styles.listDate}>2021年8月29日 19:00</Text>
+          </View>
+          <View>
+            <Text style={styles.listDelete}>✕</Text>
+          </View>
+        </View>
+        {/* item end */}
+      </View>
+
+      <View style={styles.circleButton}>
+        <Text style={styles.circleButtonText}>+</Text>
+      </View>
+
       {/* eslint-disable-next-line react/style-prop-object */}
-      <StatusBar style="auto" />
+      {/* <StatusBar style="auto" /> */}
     </View>
   );
 }
@@ -34,8 +48,68 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#F0F4F8',
+  },
+  appBar: {
+    width: '100%',
+    height: 104,
+    backgroundColor: '#467FD3',
+    justifyContent: 'flex-end',
+  },
+  appBarTitle: {
+    width: '100%',
+    textAlign: 'center',
+    color: '#FFFFFF',
+    fontSize: 22,
+    fontWeight: 'bold',
+    padding: 10,
+  },
+  appBarLogout: {
+    color: '#FFFFFF',
+    position: 'absolute',
+    right: 10,
+    bottom: 10,
+  },
+  list: {
+    backgroundColor: '#FFFFFF',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 2,
+    padding: 20,
+  },
+  listTitle: {
+    fontSize: 18,
+    marginBottom: 5,
+  },
+  listDate: {
+    color: '#999999',
+  },
+  listDelete: {
+    color: '#999999',
+    fontSize: 18,
+  },
+  circleButton: {
+    backgroundColor: '#467FD3',
+    width: 64,
+    height: 64,
+    borderRadius: 32,
     alignItems: 'center',
     justifyContent: 'center',
+    position: 'absolute',
+    right: 30,
+    bottom: 30,
+    // ios
+    shadowColor: '#000000',
+    shadowOffset: { width: 2, height: 4 },
+    shadowOpacity: 0.2,
+    shadowRadius: 4,
+    // android
+    elevation: 4,
+  },
+  circleButtonText: {
+    color: '#FFFFFF',
+    fontSize: 40,
+    lineHeight: 40,
   },
 });
